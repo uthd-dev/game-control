eba85ea98b03f518a5c94b69effa96a302b9a5e2
uthd-dev <aubin@uthd.dev>
2021-02-08 19:04:39 -0800

    Added logged-in check to nav API

--- pages/api/nav/[context].js
+++ pages/api/nav/[context].js
@@ -1,15 +1,18 @@
 import nextConnect from "next-connect";
 import middleware from "../../../middleware/index";
-import { getLinks } from "../../../utils/links"
+import { getLinks } from "../../../utils/links";
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
 handler.get(async (req, res) => {
+  if (req.user) {
     const { context } = req.query;
     const { permLevel } = req.user;
-    res.json({ nav: await getLinks(req, context, permLevel) })
+    res.json({ nav: await getLinks(req, context, permLevel) });
+  }
+  else res.json([]); 
 });
 
 export default handler;
